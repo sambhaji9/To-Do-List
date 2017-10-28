c66f276d75133491a45e3855d75b2e928a0e2e7e
Sambhaji <bbi-m1017@BBI-Ms-MacBook-Pro.local>
2017-10-28 10:24:12 +0530

    Added search box for searching tasks

--- public/javascript/servejax.js
+++ public/javascript/servejax.js
@@ -6,10 +6,21 @@ $(document).ready(function() {
     $("body").on("change", "input[type='checkbox']", function() {
         // get the row id
         var rowId = $(this).closest("tr").attr("id");
+        // get the row
+        var row = $(this).closest("tr")[0].children[1];
+
         // get the checkbox id
         var checkBox = "checkBox_".concat(rowId);
         // find out the checkbox ticked status
         var tickStatus = $("#" + checkBox).is(":checked");
+
+        // strikeout the sentence, if ticked
+        if (tickStatus) {
+            row.style.textDecoration = "line-through";
+            row.style.textDecorationColor = "red";
+        } else {
+            row.style.textDecoration = "none";
+        }
 
         var xmlHttpRequest = new XMLHttpRequest();
 
@@ -30,9 +41,39 @@ $(document).ready(function() {
         xmlHttpRequest.open("GET", "http://127.0.0.1:3000/update_task" + "?rowId=" + rowId + "&tickStatus=" + tickStatus, true);
         xmlHttpRequest.send();
     });
+
+    $("#tBodyList").click(function() {
+        console.log("clicked on td");
+    });
+
+    $("#search").keyup(function() {
+        // get the text from the search box
+        var searchText = document.getElementById("search").value;
+
+        // get the reference to the table
+        var tBodyList = document.getElementById("tBodyList");
+        // get the array of rows in the list
+        var rowArray = tBodyList.children;
+        // find count of the rows
+        var rowCount = rowArray.length;
+
+        // iterate over the array
+        for (var item = 0; item < rowCount; item++) {
+            // get the text from the row
+            var text = rowArray[item].children[1].innerHTML;
+            if (text.toLowerCase().indexOf(searchText.toLowerCase()) > -1) {
+                rowArray[item].style.display = "";
+            } else {
+                // hide the row, if text not matching
+                rowArray[item].style.display = "none";
+            }
+        }
+    });
 });
 
-
+/**
+ * function adding tasks in the list.
+ */
 function addTask() {
     var xmlHttpRequest = new XMLHttpRequest();
 
@@ -43,7 +84,9 @@ function addTask() {
     xmlHttpRequest.onreadystatechange = function() {
         if (xmlHttpRequest.readyState === XMLHttpRequest.DONE) {
             if (xmlHttpRequest.status === 200) {
+                // load the tasks list
                 loadList(xmlHttpRequest.responseText);
+                // make the input field empty
                 document.getElementById("task").value = "";
             } else {
                 console.error("This is a problem with the request");
@@ -55,6 +98,7 @@ function addTask() {
     xmlHttpRequest.send();
 }
 
+/** function getting the complete list of the tasks */
 function getTasksList() {
     var xmlHttpRequest = new XMLHttpRequest();
 
@@ -65,6 +109,7 @@ function getTasksList() {
     xmlHttpRequest.onreadystatechange = function() {
         if (xmlHttpRequest.readyState === XMLHttpRequest.DONE) {
             if (xmlHttpRequest.status === 200) {
+                // load the complete list of tasks
                 loadList(xmlHttpRequest.responseText);
             } else {
                 console.error("This is a problem with the request");
@@ -99,6 +144,12 @@ function loadList(list) {
         // create the task cell
         var taskCell = document.createElement("td");
         taskCell.innerHTML = data[node].task;
+
+        if (data[node].status === "1") {
+            taskCell.style.textDecoration = "line-through";
+            taskCell.style.textDecorationColor = "red";
+        }
+
         row.appendChild(taskCell);
 
         // create the status cell
